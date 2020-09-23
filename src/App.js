10421287f336affc1946467be7b44a9ba1db5693
A00886105 <95jvt71@gmail.com>
2020-09-22 20:41:52 -0700

    created filter variables

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import './App.css'
 import FilterButton from './components/FilterButton'
 import { nanoid } from 'nanoid'
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App (props) {
-  // state
+  // statehook
   const [tasks, setTasks] = useState(props.tasks)
+  const [filter, setFilter] = useState('All')
 
   // function that sets a state
   function addTask (name) {
@@ -42,6 +51,19 @@ function App (props) {
     setTasks(remainingTasks)
   }
 
+  // editing function for Todo component
+  function editTask (id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, name: newName }
+      }
+
+      return task
+    })
+
+    setTasks(editedTaskList)
+  }
+
   // the tasklist running through loop and return the component Todo
   const taskList = tasks.map(task => (
     <Todo
@@ -51,6 +73,16 @@ function App (props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ))
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ))
 
@@ -60,14 +92,11 @@ function App (props) {
 
   return (
     <div className='todoapp stack-large'>
-      <h1>TodoMatic</h1>
 
       <Form addTask={addTask} />
 
       <div className='filters btn-group stack-exception'>
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id='list-heading'>
         {headingText}
