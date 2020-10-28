2fa3b7b25eb6c1b552a6944b3cbe7b2986701cb4
A00886105 <95jvt71@gmail.com>
2020-10-28 11:11:08 -0700

    fixed search bar

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
 function API () {
-    const [country, setCountry] = useState([])
+    const [country, setCountry] = useState('Canada')
     const [code, setCode] = useState([])
+    const [population, setPopulation] = useState('')
     const [currency, setCurrency] = useState([])
     const [symbol, setSymbol] = useState([])
+    const [userInput, setUserInput] = useState('')
 
     async function fetchCountry(countryName) {
         // fetches the country name
@@ -35,14 +37,27 @@ function API () {
         return response.quote
     }
 
+    async function fetchCountryByCurrency(symbol) {
+        const fetchCountry = await fetch(`https://restcountries.eu/rest/v2/currency/${symbol}`)
+
+        const response = await fetchCountry.json()
+        
+        if ("status" in response) {
+            return response
+        }
+
+        return response.name
+    }
+
     useEffect(() => {
-        fetchCountry('Canada').then(resCode => {
+        fetchCountry(country).then(resCode => {
             if ("status" in resCode) {
                 return setCode(resCode.message)
             }
 
-            setCountry(resCode)
+            setCountry(resCode.name)
             setCode(resCode.currencies[0].code)
+            setPopulation(resCode.population)
             setSymbol(resCode.currencies[0].symbol)
 
             fetchCurrency(resCode.currencies[0].code).then(resCurrency => {
@@ -61,27 +76,61 @@ function API () {
 
                     arrayList.push(object)
                 })
+
+                
+
                 setCurrency(arrayList)
 
             })
         })
         
-    }, [])
+    }, [country])
 
     const currencyList = currency.map(item => (
         <p key={item.code}>{item.code}: {item.currency}</p>
     ))
 
-    return (
-    <div>
+    function handleSubmit(e) {
+        e.preventDefault()
+        setCountry(userInput)
+    }
+
+    function handleChange(e) {
+        setUserInput(e.target.value)
+    }
+
+    const result = (
         <div>
-            <h4>{code}, {country.name} with a population of {country.population} people</h4>
-            <p>Below are conversion rate of {code} = {symbol}1</p>
+            <form onSubmit={handleSubmit}>
+                <input className='input input__lg' placeholder='Canada' onChange={handleChange}/>
+                <button type='submit' className='btn btn__primary btn__lg'>
+                    Search
+                </button>
+            </form>
+            
+            <div>
+                <h4>{code}, {country} with a population of {population} people</h4>
+                <p>Below are conversion rate of {code} = {symbol}1</p>
+            </div>
+            
+            <div>{currencyList}</div>
         </div>
-        
-        <div>{currencyList}</div>
-    </div>
     )
+
+    const error = (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input className='input input__lg' placeholder='Canada' onChange={handleChange}/>
+                <button type='submit' className='btn btn__primary btn__lg'>
+                    Search
+                </button>
+            </form>
+            <h1>Error: {code}</h1>
+        </div>
+    )
+    
+    
+    return code === "Not Found" ? error : result
 }
 
 export default API
