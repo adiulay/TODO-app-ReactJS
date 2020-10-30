963f713eee026e72f3c0a22c4c175b0345c9e6a3
A00886105 <95jvt71@gmail.com>
2020-10-30 14:19:17 -0700

    replaced with redux, not todoapp

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import API from './components/Api'
-import Navbar from './components/Navbar'
-import Error from './components/Error'
+import React, { Fragment, useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import ThemeContext from './context/ThemeContext'
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
 
-function App (props) {
-  const theme = useContext(ThemeContext)
-  const [themeState, setThemeState] = useState(theme)
-
-  // localstorage used both use effect
+const App = () => {
   useEffect(() => {
-    const currentTheme = localStorage.getItem('currentTheme')
-
-    if (currentTheme) {
-      setThemeState(currentTheme)
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('currentTheme', themeState)
-  }, [themeState])
-
-  // stores state of useContext
-  const changeTheme = {
-    backgroundColor: `${themeState === "light" ? "white" : "black"}`,
-    color: `${themeState === "light" ? "black" : "white"}`,
-  }
-
-  function handleTheme () {
-    setThemeState(themeState === "light" ? "dark" : "light")
-  }
-
-  // pass object on theme
-  const theme_ = { changeTheme, handleTheme }
-  
+    // init materialize JS
+    M.AutoInit();
+  });
   return (
-    <ThemeContext.Provider value={theme_}>
-      <div className='todoapp stack-large' style={changeTheme}>
-        <Navbar />
-        <Switch>
-          <Route path='/' component={ Home } exact />
-          <Route path='/about' component = { About } />
-          <Route path='/API' component = { API } />
-          <Route component={ Error } />
-        </Switch>
-      </div>
-    </ThemeContext.Provider>
-  )
-}
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
